81e238f8c9a3b596e91eb0e24804e155e3828ebb
André Roque Matheus <amatheus@mac.com>
2018-05-08 22:57:37 -0300

    Initial Commit

---
+++ Fateconde/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fateconde
+//
+//  Created by André Roque Matheus on 08/05/2018.
+//  Copyright © 2018 André Roque Matheus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
